c3630efeb2a26f40980c372d4100ef4d0b3fd749
Ju <write0@gmail.com>
2013-09-08 04:58:02 -0700

    Fix form to php script

--- test_planner.php
+++ test_planner.php
@@ -8,5 +8,22 @@ require_once('lib/meteo.php');
 require_once('lib/ajax.php');
 
 $m = Meteo::getInstance();
+
+
+$res = $m->get_filtered_data('1001', '1015', 50, 90, false);
+$res = $m->get_filtered_data('1001', '1015', 60, 90, false);
 $res = $m->get_filtered_data('1001', '1015', 70, 90, false);
-print_r($res);
+$res = $m->get_filtered_data('1001', '1015', 80, 90, false);
+$res = $m->get_filtered_data('1001', '1015', 50, 100, false);
+$res = $m->get_filtered_data('1001', '1015', 60, 100, false);
+$res = $m->get_filtered_data('1001', '1015', 70, 100, false);
+$res = $m->get_filtered_data('1001', '1015', 80, 100, false);
+
+$res = $m->get_filtered_data('1001', '1015', 50, 90, true);
+$res = $m->get_filtered_data('1001', '1015', 60, 90, true);
+$res = $m->get_filtered_data('1001', '1015', 70, 90, true);
+$res = $m->get_filtered_data('1001', '1015', 80, 90, true);
+$res = $m->get_filtered_data('1001', '1015', 50, 100, true);
+$res = $m->get_filtered_data('1001', '1015', 60, 100, true);
+$res = $m->get_filtered_data('1001', '1015', 70, 100, true);
+$res = $m->get_filtered_data('1001', '1015', 80, 100, true);
