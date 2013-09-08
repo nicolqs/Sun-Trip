6b08dc44e87d97cfb9961d5a1734b9bb75fae0cb
Nico <nicolqs@gmail.com>
2013-09-07 18:20:42 -0700

    Icons !

--- assets/js/sun-trip.js
+++ assets/js/sun-trip.js
@@ -4,7 +4,7 @@ $(function() {
 	function initialize() {
 		var mapOptions = {
 			zoom: 3,
-			center: new google.maps.LatLng(23, -18),
+			center: new google.maps.LatLng(30, -38),
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
@@ -20,15 +20,33 @@ $(function() {
             data = JSON.parse(data);
 
             for ( var i = 0; i < 60; i++ ) {
-            	// console.log(data[i]);
+            	// console.log(data[i].current_observation);
             	if (  data[i].current_observation != undefined ) {
-	            	var myLatlng = new google.maps.LatLng(data[i].current_observation.display_location.latitude, data[i].current_observation.display_location.longitude);
+            		if ( data[i].current_observation.icon == 'clear' )  {
+            			icon = "http://icons-ak.wxug.com/i/c/k/" + 'clear.gif';
+            		}
+            		else {
+            			if ( data[i].current_observation.icon_url.indexOf("/nt_") ) {
+            				r = data[i].current_observation.icon_url.split('/');
+            				if ( ! r[6].indexOf("nt_") ) {
+            					icon = r[6].substr(3, r[6].length);
+								console.log( icon );
+            				}
+            			}
 
+            			icon = data[i].current_observation.icon_url ;
+
+
+            		// console.log(icon);
+            	}
+
+
+	            	var myLatlng = new google.maps.LatLng(data[i].current_observation.display_location.latitude, data[i].current_observation.display_location.longitude);
 					marker[i] = new google.maps.Marker({ 
 						position: myLatlng,
 						map: map,
 						title: data[i].current_observation.display_location.full,
-				        icon: 
+				        icon: icon,
 					});
 				}
 				
