a5140ac08377485da0383c0dd31544511b8d0d79
Ju <write0@gmail.com>
2013-09-08 02:00:09 -0700

    Adding filter temp + sunny to weather

--- lib/meteo.php
+++ lib/meteo.php
@@ -9,7 +9,8 @@ class Meteo {
 	/**
 	 * Attribute
 	 */
-	protected $_api_key = 'a3377ceb5414edef';
+	//	protected $_api_key = 'a3377ceb5414edef';
+	protected $_api_key = 'fa55ab773cbb5883';
 	protected $_forecast_api_endpoint;
 	protected $_meteo_api_endpoint;
 
@@ -19,6 +20,7 @@ class Meteo {
 	private function __construct() {
 		$this->_meteo_api_endpoint = 'http://api.wunderground.com/api/' . $this->_api_key . '/conditions/q/';
 		$this->_forecast_api_endpoint = 'http://api.wunderground.com/api/' . $this->_api_key . '/forecast/q/';
+		$this->_planner_api_endpoint = 'http://api.wunderground.com/api/' . $this->_api_key . '/planner_MMDDMMDD/q/';
 	}
 
 	/**
@@ -62,6 +64,22 @@ class Meteo {
 	}
 
 	/**
+	 * return "planner" meteo JSON by zmw
+	 * $dates = MMDDMMDD
+	 */
+	public function get_planner_by_dates_zmw($dates, $zmv, $decode = false)
+	{
+	  $endpoint = str_replace('MMDDMMDD', $dates, $this->_planner_api_endpoint);
+	  $url = $endpoint .  'zmw:' . $zmv . '.json';
+	  $meteo = file_get_contents($url);
+	  if ($decode)
+	    {
+	      $meteo = json_decode($meteo);
+	    }
+	  return $meteo;
+	}
+
+	/**
 	 * return forecast JSON
 	 */
 	public function getForecast( $decode = false ) {
@@ -72,25 +90,81 @@ class Meteo {
 		return $forecast;
 	}
 
+	public function get_all_cities()
+	{	  
+	  $data = array();
+	  $yaml_cities = file_get_contents(CITIES_YML);
+	  $cities = Spyc::YAMLLoad($yaml_cities);
+	  return ($cities);
+	}
+
 	/**
 	 * return forecast JSON
 	 */
 	public function get_global_data()
 	{
-		$data = array();
-		$yaml_cities = file_get_contents(CITIES_YML);
-		$cities = Spyc::YAMLLoad($yaml_cities);
-		$i = 0;
-		foreach ($cities as $c)
+	  $cities = $this->get_all_cities();
+	  foreach ($cities as $c)
+	    {
+	      $meteo = $this->get_meteo_by_zmw($c['zmw'], true);
+	      $meteo->zmw = $c['zmw'];
+	      $meteo->dataset = $c['dataset'];
+	      $data[] = $meteo;
+	    }
+	  return ($data);
+	}
+
+	/**
+	 * p is stdclass returned from api after a call to planner
+	 */
+	public function get_avg_temp_from_planner($p)
+	{
+	  $temp_high = $p->trip->temp_high->avg->F;
+	  return ($temp_high);
+	  $temp_low = $p->trip->temp_low->avg->F;
+	  $avg = intval( ((int)$temp_high + (int)$temp_low ) / 2);
+	  return ($avg);
+	}
+ 
+	public function get_filtered_data($date_start, $date_end, $min, $max, $sunonly = false)
+	{
+	  // check if it is already cached
+	  $key = 'planner_' . $date_start . $date_end . '_' . $min . '_' . $max . '_' . (($sunonly) ? 'true' : 'false');
+	  $cache = Cache::getInstance();
+	  if (($data = $cache->get($key)))
+	    {
+	      return ($data);
+	    }
+	  //
+	  $data = array();
+	  $cities = $this->get_all_cities();
+	  $i = 0;
+	  foreach ($cities as $c)
+	    {
+	      $meteo = $this->get_planner_by_dates_zmw($date_start . $date_end, $c['zmw'], true);
+	      //echo $c['dataset'] . ':' . $meteo->trip->cloud_cover->cond . "\n";
+	      $avg_temp = $this->get_avg_temp_from_planner($meteo);
+	      if ($avg_temp < $min || $avg_temp > $max)
 		{
-			$meteo = $this->get_meteo_by_zmw($c['zmw'],true);
-			$meteo->zmw = $c['zmw'];
-			$meteo->dataset = $c['dataset'];
-			// $to_remove = array( "\n" => "", "\t" => "" );
-			// $meteo = strtr( $meteo, $to_remove );
-			$data[] = $meteo;
-			// var_dump($meteo);
+		  continue;
 		}
-		return ($data);
+	      if ($sunonly)
+		{
+		  if (strstr($meteo->trip->cloud_cover->cond, 'unny') == FALSE)
+		    {
+		      continue;
+		    }
+		}
+	      //	      echo '[+] ADDING ' . $c['dataset'] . "\n";
+
+              $meteo->zmw = $c['zmw'];
+              $meteo->dataset = $c['dataset'];
+              $data[] = $meteo;
+
+	      // if ($i++ > 10) break;
+
+	    }
+	  $cache->set($key, $data);
+	  return ($data);
 	}
 }
---
+++ test_planner.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('const.php');
+require_once('lib/yml/spyc.php');
+require_once('json.php');
+require_once('lib/cache.php');
+require_once('lib/meteo.php');
+require_once('lib/ajax.php');
+
+$m = Meteo::getInstance();
+$res = $m->get_filtered_data('1001', '1015', 70, 90, true);
+print_r($res);
