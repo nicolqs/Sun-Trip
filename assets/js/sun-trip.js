1d48f897add12ddec4f866e4f4f01e3023867250
Nico <nicolqs@gmail.com>
2013-09-07 18:39:29 -0700

    G Maps little animation

--- assets/js/sun-trip.js
+++ assets/js/sun-trip.js
@@ -1,6 +1,6 @@
 $(function() {
 
-	var map;
+	var map;var line;
 	function initialize() {
 		var mapOptions = {
 			zoom: 3,
@@ -8,63 +8,79 @@ $(function() {
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+		var lineCoordinates = [
+			new google.maps.LatLng(22.291, 153.027),
+			new google.maps.LatLng(18.291, 153.027)
+		];
+
+		var lineSymbol = {
+			path: google.maps.SymbolPath.CIRCLE,
+			scale: 8,
+			strokeColor: '#393'
+		};
+
+		line = new google.maps.Polyline({
+			path: lineCoordinates,
+			icons: [{
+				icon: lineSymbol,
+				offset: '100%'
+			}],
+			map: map
+		});
+
 	}
 
+
+	function animateCircle() {
+		var count = 0;
+		offsetId = window.setInterval(function() {
+			count = (count + 1) % 200;
+
+			var icons = line.get('icons');
+			icons[0].offset = (count / 2) + '%';
+			line.set('icons', icons);
+		}, 20);
+	}
+
+	setTimeout( animateCircle, 2000 );
 	google.maps.event.addDomListener(window, 'load', initialize);
 
 	function getMeteoData() {
 		$.post( '/ajax.php', { action: "get_meteo" }, function(data){
-            // jQuery('#comments-container .fyre-stream-content').append(data);
-
             var marker = {};
             data = JSON.parse(data);
 
-            for ( var i = 0; i < 60; i++ ) {
-            	// console.log(data[i].current_observation);
-            	if (  data[i].current_observation != undefined ) {
-            		if ( data[i].current_observation.icon == 'clear' )  {
-            			icon = "http://icons-ak.wxug.com/i/c/k/" + 'clear.gif';
-            		}
-            		else {
-            			if ( data[i].current_observation.icon_url.indexOf("/nt_") ) {
-            				r = data[i].current_observation.icon_url.split('/');
-            				if ( ! r[6].indexOf("nt_") ) {
-            					icon = r[6].substr(3, r[6].length);
+			for ( var i = 0; i < 60; i++ ) {
+				if (  data[i].current_observation != undefined ) {
+					if ( data[i].current_observation.icon == 'clear' )  {
+						icon = "http://icons-ak.wxug.com/i/c/k/" + 'clear.gif';
+					}
+					else {
+						if ( data[i].current_observation.icon_url.indexOf("/nt_") ) {
+							r = data[i].current_observation.icon_url.split('/');
+							if ( ! r[6].indexOf("nt_") ) {
 								console.log( icon );
-            				}
-            			}
+								icon = r[6].substr(3, r[6].length);
+							} else {
+								icon = data[i].current_observation.icon_url ;
+							}
+						}
 
-            			icon = data[i].current_observation.icon_url ;
+						icon = data[i].current_observation.icon_url ;
+				}
 
 
-            		// console.log(icon);
-            	}
-
-
-	            	var myLatlng = new google.maps.LatLng(data[i].current_observation.display_location.latitude, data[i].current_observation.display_location.longitude);
+					var myLatlng = new google.maps.LatLng(data[i].current_observation.display_location.latitude, data[i].current_observation.display_location.longitude);
 					marker[i] = new google.maps.Marker({ 
 						position: myLatlng,
 						map: map,
 						title: data[i].current_observation.display_location.full,
-				        icon: icon,
+						icon: icon,
 					});
 				}
 				
             }
-			// $.each(data, function (i, val) {
-			// 	// console.log( val );
-
-			// 	var myLatlng = new google.maps.LatLng(val.current_observation.display_locaion.latitude, val.current_observation.display_locaion.latitude);
-
-			// 	marker[i] = new google.maps.Marker({ 
-			// 		position: myLatlng,
-			// 		map: map,
-			// 		title: val.current_observation.display_locaion.full,
-			// 	});
-
-			// });
-
-
         });
 	}
 
